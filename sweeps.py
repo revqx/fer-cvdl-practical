56bb24c3a499f27613b31ae9e3263db90fae0c86
E-Schmitz <112330300+E-Schmitz@users.noreply.github.com>
2024-01-24 23:09:49 +0100

    Updated Sweeps
    added val_loss tracking
    validation split parameter
    weak_class_adjust parameter

--- sweeps.py
+++ sweeps.py
@@ -1,20 +1,20 @@
-import numpy as np  # needed for custom train method
-import torch  # needed for device and custom train method
-import torch.nn.functional as F  # needed for custom train method
+import numpy as np  
+import torch  
+import torch.nn.functional as F  
+from sklearn.model_selection import train_test_split  
 
-import torch.nn as nn  # needed for dynamic model
+import torch.nn as nn  
 import wandb
-from torch.utils.data import DataLoader, WeightedRandomSampler  # needed for custom train method
+from torch.utils.data import DataLoader, WeightedRandomSampler  
 
-from dataset import get_dataset  # needed for custom train method
-from model import get_model, _create_conv_block, _create_conv_block_2  # needed for custom train method
-from model import get_model  # needed for custom train method
-from preprocessing import select_preprocessing  # needed for custom train method
+from dataset import get_dataset  
+from model import get_model, _create_conv_block, _create_conv_block_2  
+from model import get_model  
+from preprocessing import select_preprocessing 
 
 
-def get_sweep_config(metric="loss", goal="minimize", method="random",
+def get_sweep_config(metric="val_loss", goal="minimize", method="random",
                      custom_model=True, early_terminate=None):
-                     custom_model=False, early_terminate=None):
       
     sweep_config = {
         "method": method  # to be specified by user
@@ -29,29 +29,22 @@ def get_sweep_config(metric="loss", goal="minimize", method="random",
     # parameters to sweep over (dropout not possible atm because models need custom input for dropout)
     parameters_dict = {
         "optimizer": {
-
-            "values": ['sgd']  # options: adam, sgd
+        "values": ['sgd']  # options: adam, sgd
         },
         "dataset": {
             "values": ["RAF-DB"]  # options: AffectNet, RAF-DB
         },
         "batch_size": {
             "values": [16, 24, 32]  # defined here since log distribution causes bad comparability
+        },
+        "validation_split": {
+            "values": [0.1, 0.2] # once sweeped to be set as constant
+        },
+        "weak_class_adjust": {
+            "values": [1]  # can be set to True and weights have to be adjusted in get_sweep_loader
         }
-
-            "values": ['sgd', 'adam']  # options: adam, sgd
-        },
-        "dataset": {
-            "values": ["RAF-DB", "AffectNet"]  # options: AffectNet, RAF-DB
-        },
-        "batch_size": {
-            "values": [16, 24, 32, 64, 128]  # defined here since log distribution causes bad comparability
-        },
-        "model_name": {
-            "values": ["model_name"]
-        }  # options: EmotionModel_2, CustomEmotionModel_3, LeNet, ResNet18
-
     }
+
     sweep_config["parameters"] = parameters_dict
 
     if custom_model:
@@ -106,7 +99,7 @@ def train_sweep(custom_model=True):
         config = wandb.config
 
         # get dataset and dataloaders (val_loader not needed for now)
-        loader = get_sweep_loader(config)
+        train_loader, val_loader = get_sweep_loader(config)
 
         # get model
         if custom_model:
@@ -119,11 +112,15 @@ def train_sweep(custom_model=True):
         optimizer = get_optimizer_sweep(config["optimizer"], model, config.learning_rate)
 
         for epoch in range(config["epochs"]):
-            avg_loss = train_epoch(model, loader, optimizer, device)
-            wandb.log({"loss": avg_loss, "epoch": epoch})
+            avg_loss = train_epoch(model, train_loader, optimizer, device)
+            wandb.log({"train_loss": avg_loss, "epoch": epoch})
+
+            val_loss = validate(model, val_loader, device)
+            wandb.log({"val_loss": val_loss, "epoch": epoch})
 
 
 def train_epoch(model, loader, optimizer, device):
+    model.train()  
     cumu_loss = 0
     for _, (data, target) in enumerate(loader):
         data, target = data.to(device), target.to(device)
@@ -131,8 +128,8 @@ def train_epoch(model, loader, optimizer, device):
 
         # ➡ Forward pass
         output = model(data)
-        log_output = F.log_softmax(output, dim=1)  # Apply log-softmax to the output
-        loss = F.nll_loss(log_output, target)  # Compute the loss using the log-softmax output
+        log_output = F.log_softmax(output, dim=1) 
+        loss = F.nll_loss(log_output, target)  # loss calculation interchangeable
         cumu_loss += loss.item()
 
         # ⬅ Backward pass + weight update
@@ -140,6 +137,21 @@ def train_epoch(model, loader, optimizer, device):
         optimizer.step()
 
         wandb.log({"batch loss": loss.item()})
+
+    return cumu_loss / len(loader)
+
+
+def validate(model, loader, device):
+    model.eval() 
+    cumu_loss = 0
+    with torch.no_grad(): 
+        for _, (data, target) in enumerate(loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            log_output = F.log_softmax(output, dim=1)
+            loss = F.nll_loss(log_output, target)  
+            cumu_loss += loss.item()
 
     return cumu_loss / len(loader)
 
@@ -163,13 +175,25 @@ def get_sweep_loader(config):
     class_counts = np.bincount(y)
     class_weights = 1. / class_counts
     weights = class_weights[y]
-    # Create the sampler
+
+    # Increase the weight of the fear class
+    fear_class = 2  # the class index should be 2
+    weights[y == fear_class] *= config["weak_class_adjust"]
+    disgust_class = 1
+    weights[y == disgust_class] *= config["weak_class_adjust"]
+
+    # create the sampler
     sampler = WeightedRandomSampler(weights, len(weights))
+    loader = DataLoader(dataset, batch_size=len(dataset), sampler=sampler)
+    # has to be turned back into a dataset to use train_test_split
+    uniform_dataset, uniform_labels = next(iter(loader))
 
-    # Create the dataloaders
-    loader = DataLoader(dataset, batch_size=config["batch_size"], sampler=sampler)
+    train_data, val_data, train_labels, val_labels = train_test_split(uniform_dataset, uniform_labels, test_size=config["validation_split"], stratify=uniform_labels)
 
-    return loader
+    train_loader = DataLoader(list(zip(train_data, train_labels)), batch_size=config["batch_size"])
+    val_loader = DataLoader(list(zip(val_data, val_labels)), batch_size=config["batch_size"], shuffle=False)
+
+    return train_loader, val_loader
 
 
 class DynamicModel(nn.Module):
