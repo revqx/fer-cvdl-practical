5a50a0c5d6a446b31a91ba41f89b3531106e4101
Daniel Schuster <daniel.a.schuster@t-online.de>
2024-01-29 12:27:12 +0100

    Add information

--- video_prediction.py
+++ video_prediction.py
@@ -1,12 +1,16 @@
-import os
-
 import cv2
 import torch
 
 from inference import load_model_and_preprocessing
 from utils import LABEL_TO_STR
 
-FACE_CASCADE_PATH = "cascades/haarcascade_frontalface_default.xml"
+BLUE_COLOR = (255, 0, 0)
+RED_COLOR = (0, 0, 255)
+GREEN_COLOR = (0, 255, 0)
+WHITE_COLOR = (255, 255, 255)
+INFO_TEXT_SIZE = 0.7
+DEFAULT_FONT = cv2.FONT_HERSHEY_SIMPLEX
+
 
 def initialize_model(model_name: str):
     """Initialize the model with the given name"""
@@ -16,6 +20,7 @@ def initialize_model(model_name: str):
     model.to(device)
 
     return model, preprocessing, device
+
 
 def initialize_cap(webcam: bool, video_input: str):
     """Initialize the camera or video input"""
@@ -32,7 +37,8 @@ def initialize_cap(webcam: bool, video_input: str):
 
     return cap
 
-def initialize_out(cap, file, codec='XVID'):
+
+def initialize_out(cap, file, codec="MJPG"):
     """Initialize the video output"""
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
@@ -41,49 +47,144 @@ def initialize_out(cap, file, codec='XVID'):
     fourcc = cv2.VideoWriter_fourcc(*codec)
     return cv2.VideoWriter(file, fourcc, fps, (width, height))
 
-def predict_emotion(image, model, preprocessing, device):
-    """Predict the emotion of the given image"""
+
+def top_prediction_with_label(predictions):
+    """Return the top prediction with its label"""
+    top_prediction = torch.argmax(predictions, 1)
+    label = LABEL_TO_STR[top_prediction.item()]
+
+    score = predictions[0][top_prediction].item()
+    return label, score
+
+
+def predict_emotions(image, model, preprocessing, device):
+    """Predict the emotions of the given image"""
     resized_image = cv2.resize(image, (64, 64))
     normalized_image = resized_image / 255.0
-    torch_image = torch.from_numpy(normalized_image).unsqueeze(0).permute(0, 3, 1, 2).float().to(device)
+    torch_image = (
+        torch.from_numpy(normalized_image)
+        .unsqueeze(0)
+        .permute(0, 3, 1, 2)
+        .float()
+        .to(device)
+    )
 
     if preprocessing:
         torch_image = preprocessing(torch_image)
 
     with torch.no_grad():
         output = model(torch_image)
-        predictions = torch.nn.functional.softmax(output, dim=1) 
-        predicted_class = torch.argmax(predictions, 1)
-        emotion = LABEL_TO_STR[predicted_class.item()]
-        
-        score = predictions[0][predicted_class].item()
-        
-    return emotion, score
+        predictions = torch.nn.functional.softmax(output, dim=1)
+
+    return predictions
+
 
 def process_frame(frame, face_cascade, model, device, preprocessing, emotion_score):
     """Process the frame and return the frame with the predicted emotions"""
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    RECTANGLE_COLOR = (255, 0, 0)
+    cv2.putText(
+        frame,
+        f"Number of faces detected: {len(faces)}",
+        (10, 30),
+        DEFAULT_FONT,
+        1.1,
+        WHITE_COLOR,
+        2,
+    )
 
-    for (x, y, w, h) in faces:
-        emotion, score = predict_emotion(frame[y:y + h, x:x + w], model, preprocessing, device)
-        
-        emotion_score[emotion]['total_score'] += score
-        emotion_score[emotion]['count'] += 1
-    
-        cv2.rectangle(frame, (x, y), (x + w, y + h), RECTANGLE_COLOR, 2)
-        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, RECTANGLE_COLOR, 2)
+    offset = 60
+    for face_nr, (x, y, w, h) in enumerate(faces):
+        id = face_nr + 1
 
+        roi = frame[y : y + h, x : x + w]
+
+        predictions = predict_emotions(roi, model, preprocessing, device)
+        emotion, score = top_prediction_with_label(predictions)
+
+        emotion_score[emotion]["total_score"] += score
+        emotion_score[emotion]["count"] += 1
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), BLUE_COLOR, 2)
+
+        dynamic_text_size = max(min(h / 300, 1), 0.5)
+        cv2.putText(
+            frame,
+            f"Face #{id}: {emotion}",
+            (x, y - 10),
+            DEFAULT_FONT,
+            dynamic_text_size,
+            BLUE_COLOR,
+            2,
+        )
+
+        eye_cascade = cv2.CascadeClassifier(
+            cv2.data.haarcascades + "haarcascade_eye.xml"
+        )
+        smile_cascade = cv2.CascadeClassifier(
+            cv2.data.haarcascades + "haarcascade_smile.xml"
+        )
+
+        show_info_box = True
+        show_details = True
+        if show_details:
+            mouths = smile_cascade.detectMultiScale(roi, 1.6, 20)
+
+            if len(mouths) > 0:
+                (mx, my, mw, mh) = mouths[0]
+                cv2.ellipse(
+                    roi,
+                    (mx + mw // 2, my + mh // 2),
+                    (mw // 2, mh // 2),
+                    0,
+                    0,
+                    360,
+                    RED_COLOR,
+                    2,
+                )
+
+            eyes = eye_cascade.detectMultiScale(roi, 1.8, 10)
+            for x2, y2, w2, h2 in eyes[:2]:
+                eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)
+                radius = int(round((w2 + h2) * 0.25))
+                cv2.circle(frame, eye_center, radius, RED_COLOR, 4)
+
+        if show_info_box:
+            cv2.rectangle(frame, (10, offset), (200, offset + 150), WHITE_COLOR, 2)
+            cv2.putText(
+                frame,
+                f"Face #{id}",
+                (15, offset + 20),
+                DEFAULT_FONT,
+                INFO_TEXT_SIZE,
+                WHITE_COLOR,
+                2,
+            )
+            for i, (emotion, score) in enumerate(
+                zip(LABEL_TO_STR.values(), predictions[0])
+            ):
+                cv2.putText(
+                    frame,
+                    f"{emotion}: {score:.2f}",
+                    (15, offset + 40 + i * 20),
+                    DEFAULT_FONT,
+                    INFO_TEXT_SIZE,
+                    WHITE_COLOR,
+                    2,
+                )
+
+        offset += 150
     return frame
 
 
-def main_loop(cap, face_cascade, model, device, preprocessing, show_processing):
+def main_loop(cap, face_cascade, model, device, preprocessing, show_processing, webcam):
     """Main loop for video prediction"""
     print("Starting video prediction...")
-    
-    emotion_scores = {emotion: {'total_score': 0, 'count': 0} for emotion in LABEL_TO_STR.values()}
+
+    emotion_scores = {
+        emotion: {"total_score": 0, "count": 0} for emotion in LABEL_TO_STR.values()
+    }
 
     while True:
         has_frame, frame = cap.read()
@@ -91,39 +192,59 @@ def main_loop(cap, face_cascade, model, device, preprocessing, show_processing):
         if not has_frame:
             break
 
-        
-        processed_frame = process_frame(frame, face_cascade, model, device, preprocessing, emotion_scores)
-        if show_processing:
-            cv2.imshow('Facial Emotion Recognition', processed_frame)
+        if webcam:
+            frame = cv2.flip(frame, 1)
 
-        q_pressed = cv2.waitKey(1) == ord('q')
-        window_closed = show_processing and cv2.getWindowProperty('Facial Emotion Recognition',
-                                                                  cv2.WND_PROP_VISIBLE) < 1
+        processed_frame = process_frame(
+            frame, face_cascade, model, device, preprocessing, emotion_scores
+        )
+
+        if show_processing:
+            cv2.imshow("Facial Emotion Recognition", processed_frame)
+
+        q_pressed = cv2.waitKey(1) == ord("q")
+        window_closed = (
+            show_processing
+            and cv2.getWindowProperty(
+                "Facial Emotion Recognition", cv2.WND_PROP_VISIBLE
+            )
+            < 1
+        )
         if q_pressed or window_closed:
             print("Video prediction interrupted.")
             break
-    
+
     for emotion, score in emotion_scores.items():
-        average_score = score['total_score'] / score['count'] if score['count'] > 0 else 0
+        average_score = (
+            score["total_score"] / score["count"] if score["count"] > 0 else 0
+        )
         print(f"Emotion: {emotion}, Average Score: {average_score:.2f}")
 
-def make_video_prediction(model_name: str, record: bool, webcam: bool, video_input: str, output_file: str, show_processing: bool):
-    """Make video prediction using the model with the given name"""
-    if not os.path.isfile(FACE_CASCADE_PATH):
-        raise IOError(
-            "Please download the haar cascade file from https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml and put it into /cascades")
 
-    face_cascade = cv2.CascadeClassifier(FACE_CASCADE_PATH)
+def make_video_prediction(
+    model_name: str,
+    record: bool,
+    webcam: bool,
+    video_input: str,
+    output_file: str,
+    show_processing: bool,
+):
+    """Make video prediction using the model with the given name"""
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
 
     model, preprocessing, device = initialize_model(model_name)
 
     cap = initialize_cap(webcam, video_input)
-    
+
     if record:
         out = initialize_out(cap, output_file)
 
     try:
-        main_loop(cap, face_cascade, model, device, preprocessing, show_processing)
+        main_loop(
+            cap, face_cascade, model, device, preprocessing, show_processing, webcam
+        )
     except Exception as e:
         print(e)
     finally:
